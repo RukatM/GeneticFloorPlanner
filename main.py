6c354e9bd2304f047f8e54e220a01ee704e1cc5e
Maciej Rukat <beton@fedora.home>
2025-06-07 01:26:30 +0200

    Wstepna inicjacja populacji

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from inout.parser import parse_input_file
 from genetic.operators import initialize_population
 from genetic.evaluator import calculate_fitness
 
-def run_optimization():
+def run_optimization(num_generations,population_size):
     """
     Main function to run the genetic algorithm optimization.
     """
@@ -18,20 +18,30 @@ def run_optimization():
     print("Configuration loaded successfully")
 
     print("Initializing population")
-    population = initialize_population(config_data,5)
+    population = initialize_population(config_data,population_size)
 
-    if population:
+    if not population:
+        print("Population initialisation failed")
+        sys.exit(2) 
+
+    
+    else:
         print("Population succefully initialised")
 
-        for individual in population:
-            for chromosome in individual.chromosomes:
-                print(chromosome)
-            score = calculate_fitness(individual,config_data)
-            print("Fitness: ",score)
-            individual.fitness = score
+        for generation in range(num_generations):
+            for individual in population:
+                individual.fitness = calculate_fitness(individual,config_data)
 
-    else:
-        print("Population initialisation failed")
+            best_individual = max(population,key = lambda individual : individual.fitness)
+            print(f"Generacja:{generation + 1} najlepszy osobnik: {best_individual}")
+
+
+        # for individual in population:
+        #     for chromosome in individual.chromosomes:
+        #         print(chromosome)
+        #     score = calculate_fitness(individual,config_data)
+        #     print("Fitness: ",score)
+        #     individual.fitness = score
 
 if __name__ == "__main__":
-    run_optimization()
+    run_optimization(100,5)
